e82ca71387e17cde596bc31796f236b518ad6faa
dev-ankit24 <ankitkumar839282@gmail.com>
2025-09-16 15:48:51 +0530

    add kailsh

--- src/app/infrastructure-solutions/page.js
+++ src/app/infrastructure-solutions/page.js
@@ -1,4 +1,5 @@
-import InfrastructurePage from '@/components/infrastructure-solutions/InfrastructurePage'
+
+import InfrastructurePage from '@/components/infrastructure-solutionspage/InfrastructurePage'
 import React from 'react'
 
 export default function page() {
